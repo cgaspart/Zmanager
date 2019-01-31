daac1e9e5e0e470461f5c2c2541443019d378bbd
Lox3ur <cgaspart@student.42.fr>
2019-01-31 19:34:41 +0000

    Sku*2

--- client/src/App.js
+++ client/src/App.js
@@ -39,32 +39,16 @@ render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <p>{this.state.response}</p>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Post to Server:</strong>
-          </p>
-          <input
-            type="text"
-            value={this.state.post}
-            onChange={e => this.setState({ post: e.target.value })}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <p>{this.state.responseToPost}</p>
+        	<p>{this.state.response}</p>
+        	<form onSubmit={this.handleSubmit}>
+          		<p>
+            			<strong>Post to Server:</strong>
+          		</p>
+          		<input type="text" value={this.state.post} onChange={e => this.setState({ post: e.target.value })}/>
+          		<button type="submit">Submit</button>
+        	</form>
+        	<p>{this.state.responseToPost}</p>
+	</header>
       </div>
     );
   }
