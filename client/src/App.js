04e2613033e1f8367d5a93064ab8fe07cfb8a6d5
Lox3ur <cgaspart@student.42.fr>
2019-02-03 19:05:38 +0100

    Clean code

--- client/src/App.js
+++ client/src/App.js
@@ -10,55 +10,11 @@ class App extends Component {
     responseToPost: '',
   };
 
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.result }))
-      .catch(err => console.log(err));
-  }
-
-  callApi = async () => {
-    const response = await fetch('/api/nodeSaved');
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    return body;
-  };
-
-  deleteNode = async () => {
-    const response = await fetch('/api/rmExemple');
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    return body;
-  }
-
-  handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('/api/world', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ post: this.state.post }),
-    });
-    const body = await response.text();
-    this.setState({ responseToPost: body });
-  };
 
 render() {
     return (
       <div className="App">
             <Route path="/" component={menu} />
-        <header className="App-header">
-        	<p>{this.state.response}</p>
-        	<form onSubmit={this.handleSubmit}>
-          		<p>
-            			<strong>Post to Server:</strong>
-          		</p>
-          		<input type="text" value={this.state.post} onChange={e => this.setState({ post: e.target.value })}/>
-          		<button type="submit">Submit</button>
-        	</form>
-          <button type="button" onClick={this.deleteNode}>Delete</button>
-        	<p>{this.state.responseToPost}</p>
-	</header>
       </div>
     );
   }
@@ -75,6 +31,5 @@ export const createApp = () => {
     }
   };
 };
-
 
 export default App;
